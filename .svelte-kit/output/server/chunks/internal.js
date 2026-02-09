9f8bf61607e99da8669e9918f80474223959e16a
cdcomey <charlesdcomey@gmail.com>
2026-02-09 13:27:50 -0800

    various changes

--- .svelte-kit/output/server/chunks/internal.js
+++ .svelte-kit/output/server/chunks/internal.js
@@ -2649,7 +2649,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "zn4frw"
+  version_hash: "1spjrlr"
 };
 async function get_hooks() {
   let handle;
--- .svelte-kit/output/server/manifest-full.js
+++ .svelte-kit/output/server/manifest-full.js
@@ -7,10 +7,10 @@ function __memo(fn) {
 return {
 	appDir: "_app",
 	appPath: "portfolio_website/_app",
-	assets: new Set([".DS_Store","favicon.ico","favicon_io/.DS_Store","favicon_io/about.txt","favicon_io/android-chrome-192x192.png","favicon_io/android-chrome-512x512.png","favicon_io/apple-touch-icon.png","favicon_io/favicon-16x16.png","favicon_io/favicon-32x32.png","favicon_io/site.webmanifest","favicon_io.zip"]),
-	mimeTypes: {".txt":"text/plain",".png":"image/png",".webmanifest":"application/manifest+json",".zip":"application/zip"},
+	assets: new Set([".DS_Store",".nojekyll","favicon.ico"]),
+	mimeTypes: {},
 	_: {
-		client: {start:"_app/immutable/entry/start.Cggv2nQc.js",app:"_app/immutable/entry/app.Jaw70hXx.js",imports:["_app/immutable/entry/start.Cggv2nQc.js","_app/immutable/chunks/CfNSYe8x.js","_app/immutable/chunks/5JQ8Pm-n.js","_app/immutable/chunks/CvcC6P8I.js","_app/immutable/entry/app.Jaw70hXx.js","_app/immutable/chunks/5JQ8Pm-n.js","_app/immutable/chunks/B8HxeWkk.js","_app/immutable/chunks/CLDMBHh4.js","_app/immutable/chunks/CvcC6P8I.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.CoXsFeEI.js",app:"_app/immutable/entry/app.52Cy27iU.js",imports:["_app/immutable/entry/start.CoXsFeEI.js","_app/immutable/chunks/Dc4uJae_.js","_app/immutable/chunks/5JQ8Pm-n.js","_app/immutable/chunks/CvcC6P8I.js","_app/immutable/entry/app.52Cy27iU.js","_app/immutable/chunks/5JQ8Pm-n.js","_app/immutable/chunks/B8HxeWkk.js","_app/immutable/chunks/CLDMBHh4.js","_app/immutable/chunks/CvcC6P8I.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
