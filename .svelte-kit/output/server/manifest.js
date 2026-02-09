aa401b9c4acbeeef2f26ad2dfb257a5d809505e7
cdcomey <charlesdcomey@gmail.com>
2026-02-09 13:37:17 -0800

    clean git index and remove submodule references

--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set([".DS_Store",".nojekyll","favicon.ico"]),
 	mimeTypes: {},
 	_: {
-		client: {start:"_app/immutable/entry/start.CoXsFeEI.js",app:"_app/immutable/entry/app.52Cy27iU.js",imports:["_app/immutable/entry/start.CoXsFeEI.js","_app/immutable/chunks/Dc4uJae_.js","_app/immutable/chunks/5JQ8Pm-n.js","_app/immutable/chunks/CvcC6P8I.js","_app/immutable/entry/app.52Cy27iU.js","_app/immutable/chunks/5JQ8Pm-n.js","_app/immutable/chunks/B8HxeWkk.js","_app/immutable/chunks/CLDMBHh4.js","_app/immutable/chunks/CvcC6P8I.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.C0cMw6lw.js",app:"_app/immutable/entry/app.B_xr5a2C.js",imports:["_app/immutable/entry/start.C0cMw6lw.js","_app/immutable/chunks/Bwxo8F4K.js","_app/immutable/chunks/5JQ8Pm-n.js","_app/immutable/chunks/CvcC6P8I.js","_app/immutable/entry/app.B_xr5a2C.js","_app/immutable/chunks/5JQ8Pm-n.js","_app/immutable/chunks/B8HxeWkk.js","_app/immutable/chunks/CLDMBHh4.js","_app/immutable/chunks/CvcC6P8I.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js'))
--- .svelte-kit/output/server/nodes/1.js
+++ .svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.BxzDSHKC.js","_app/immutable/chunks/CLDMBHh4.js","_app/immutable/chunks/5JQ8Pm-n.js","_app/immutable/chunks/BnIksHCF.js","_app/immutable/chunks/B8HxeWkk.js","_app/immutable/chunks/BaMWO-ro.js","_app/immutable/chunks/Dc4uJae_.js","_app/immutable/chunks/CvcC6P8I.js"];
+export const imports = ["_app/immutable/nodes/1.B0UNr9Wb.js","_app/immutable/chunks/CLDMBHh4.js","_app/immutable/chunks/5JQ8Pm-n.js","_app/immutable/chunks/BnIksHCF.js","_app/immutable/chunks/B8HxeWkk.js","_app/immutable/chunks/BaMWO-ro.js","_app/immutable/chunks/Bwxo8F4K.js","_app/immutable/chunks/CvcC6P8I.js"];
 export const stylesheets = [];
 export const fonts = [];
